bbcf4cad98bb463c119970ad3ccd61e5f85f47e8
Raquel Bulhões <138785661+Kelzinha000@users.noreply.github.com>
2024-04-11 21:30:29 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+const App = () => {
+    return(
+       <body >
+        <div className='container'> 
+        <h1>FAQs</h1>
+        <p>What is Frontend</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea vitae sed ipsam maxime? Exercitationem asperiores consectetur cum, veritatis architecto in? Sint, possimus commodi. Tempore labore placeat esse? Eaque, nihil explicabo.</p>
+        
+        </div>
+       </body>
+
+    )
+}
+
+export default App;
